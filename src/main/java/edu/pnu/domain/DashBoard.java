c2a6c53d4a5500a339a54f17ea07a3f08f0d6936
cncn0069 <cncn0069@naver.com>
2025-06-11 17:10:01 +0900

    form

--- src/main/java/edu/pnu/domain/DashBoard.java
+++ src/main/java/edu/pnu/domain/DashBoard.java
@@ -3,6 +3,7 @@ package edu.pnu.domain;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,6 +28,7 @@ public class DashBoard {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long dash_id;
+	//@ManyToOne(fetch = FetchType.LAZY)
 	@ManyToOne
 	@JoinColumn(name = "username")
 	private Member username;
