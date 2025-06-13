72e1803c9d4e1423482d07c2d5fa5230a318e43e
cncn0069 <cncn0069@naver.com>
2025-06-13 18:11:52 +0900

    abcd

--- src/main/java/edu/pnu/persistence/DashBoardRepository.java
+++ src/main/java/edu/pnu/persistence/DashBoardRepository.java
@@ -8,8 +8,11 @@ import org.springframework.data.jpa.repository.Query;
 import edu.pnu.domain.DashBoard;
 
 public interface DashBoardRepository extends JpaRepository<DashBoard, Long>{
-	@Query("SELECT d FROM DashBoard d where d.enabled = true order by createdAt desc")
+	@Query("SELECT d FROM DashBoard d where d.enabled = true")
 	Page<DashBoard> getDashBoardAllEnabledNotFalse(Pageable pageable);
+	
+	@Query("SELECT d FROM DashBoard d where d.title Like CONCAT(:q,'%') and d.enabled = true")
+	Page<DashBoard> getDashBoardByQAndEnabledNotFalse(Pageable pageable,String q);
 	
 //	@Query("select new edu.pnu.dto.DashBoardDto(d.dashId, d.title, d.content, d.username, d.nickname, d.createdAt) from DashBoard d")
 	@Query("SELECT d FROM DashBoard d where d.dash_id = :id and d.enabled = true")
