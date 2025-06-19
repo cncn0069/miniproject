41230ae8140628fa30bb16ccdd5e91086b766d44
cncn0069 <cncn0069@naver.com>
2025-06-19 17:46:19 +0900

    order

--- src/main/java/edu/pnu/persistence/OrderItemRepository.java
+++ src/main/java/edu/pnu/persistence/OrderItemRepository.java
@@ -9,6 +9,6 @@ import edu.pnu.domain.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
 	
-	@Query("select i from OrderItem i where i.orderId = :orderId")
+	@Query("select i from OrderItem i where i.orderId.orderId = :orderId")
 	List<OrderItem> getOrderItemByOrderId(Long orderId);
 }
