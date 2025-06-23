ae83a8fa84cfc1593ffc67941f0fdc19f1d5eda8
AR384 <wjdqor0060@naver.com>
2025-06-23 13:02:03 +0900

    new2

--- src/main/java/edu/pnu/service/FileService.java
+++ src/main/java/edu/pnu/service/FileService.java
@@ -21,37 +21,55 @@ import edu.pnu.util.ServerIpUtil;
 
 @Service
 public class FileService {
-	@Value("${file.upload.directory}")
-	private String fileUploadPath;
-	@Autowired
-	private ImageFileRepository imageFileRepository;
+   @Value("${file.upload.directory}")
+   private String fileUploadPath;
+   @Value("${file.permit.directory}")
+   private String permitedImagePath;
+   @Autowired
+   private ImageFileRepository imageFileRepository;
+   
 
-	
-	public String uploadImage(MultipartFile image) throws Exception{
-		String fileName = UUID.randomUUID() + "_" + image.getOriginalFilename();
-		String filePath = fileUploadPath + "/images/" +fileName;
-		image.transferTo(new File(filePath));
-		
-		ImageFile imageFile = new ImageFile();
-		imageFile.setFileName(fileName);
-		imageFile.setFilePath(filePath);
-		imageFile.setCreated_at(LocalDateTime.now());
-		imageFileRepository.save(imageFile);
-		
-		ServerIpUtil ipUtil = new ServerIpUtil();
-		
-		return "http://"+ ipUtil.getServerIp() + ":8080"+ "/images/" + fileName;
-	}
-	
-	public ResponseEntity<byte[]> getImage(String imageName) throws IOException{
-		File imageFile = new File(fileUploadPath + "/images/" +imageName);
-		byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
-		
-		String contentType = Files.probeContentType(imageFile.toPath());
-		
-		return ResponseEntity.ok()
-				.contentType(MediaType.parseMediaType(contentType))
-				.body(imageBytes);
-		
-	}
+   
+   public String uploadImage(MultipartFile image) throws Exception{
+      String fileName = UUID.randomUUID() + "_" + image.getOriginalFilename();
+      String filePath = fileUploadPath + "/images/" +fileName;
+      image.transferTo(new File(filePath));
+      
+      ImageFile imageFile = new ImageFile();
+      imageFile.setFileName(fileName);
+      imageFile.setFilePath(filePath);
+      imageFile.setCreated_at(LocalDateTime.now());
+      imageFileRepository.save(imageFile);
+      
+      ServerIpUtil ipUtil = new ServerIpUtil();
+      
+      return "http://"+ ipUtil.getServerIp() + ":8080"+ "/images/" + fileName;
+   }
+   
+   public ResponseEntity<byte[]> getImage(String imageName) throws IOException{
+      File imageFile = new File(fileUploadPath + "/images/" +imageName);
+      byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
+      
+      String contentType = Files.probeContentType(imageFile.toPath());
+      
+      return ResponseEntity.ok()
+            .contentType(MediaType.parseMediaType(contentType))
+            .body(imageBytes);
+      
+   }
+   
+   
+   
+   public ResponseEntity<byte[]> getProcessedImage(String imageName) throws IOException{
+	   
+      File imageFile = new File(permitedImagePath + "/permit/" +imageName);
+      byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
+      
+      String contentType = Files.probeContentType(imageFile.toPath());
+      
+      return ResponseEntity.ok()
+            .contentType(MediaType.parseMediaType(contentType))
+            .body(imageBytes);
+      
+   }
 }
